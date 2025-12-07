a63c0560c9e6b135f9aacb444b6010faea45069d
Sasha <sashabeam23@gmail.com>
2025-12-07 20:16:21 +0000

    day 7 part 2

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,6 +1,3 @@
-#[allow(unused_imports)]
-use std::time::Duration;
-
 fn main() {
     println!("Advent of Code 2025, Day 05");
     let path = match std::env::args().nth(1) {
@@ -30,6 +27,7 @@ fn main() {
             grid[y][x] = input_chars[x + y*width]
         }
     }
+    let grid_backup = grid.clone(); // save the unmodified grid for part 2
 
     // grid[][] now has everything :)
     'going_down: for y in (0..height).step_by(2) {
@@ -48,24 +46,35 @@ fn main() {
                 }
             }
         }
-        // print_grid(&grid);
-        // std::thread::sleep(Duration::from_millis(100))
     }
 
     let part1_time = now.elapsed().as_micros();
     println!("Day 07 part 1: {solution} in {part1_time}µs");
-}
 
-#[allow(dead_code)]
-fn print_grid(grid: &Vec<Vec<char>>) {
-    let mut final_string = String::new();
-    for row in grid {
-        for c in row {
-            final_string.push(*c);
+
+    // part 2
+    let now = std::time::Instant::now();
+    // do the same as part 1, but it's also a goobey floodfill
+    let mut flood = vec![vec![0u64; width]; height];
+    let splitters = grid_backup; // nicer name so it isn't confusing up top
+    let x = splitters[0].iter().position(|c| *c == 'S').unwrap();
+    flood[1][x] += 1;
+    for y in (2..height).step_by(2) {
+        for x in 0..width {
+            if flood[y-1][x] > 0 {
+                match splitters[y][x] {
+                    '.' => flood[y+1][x] += flood[y-1][x],
+                    '^' => {
+                        flood[y+1][x-1] += flood[y-1][x];
+                        flood[y+1][x+1] += flood[y-1][x];
+                    }
+                    _ => {}
+                }
+            }
         }
-        final_string.push('\n');
     }
-    let footer = String::from_utf8(vec![b'_'; grid[0].len()]).unwrap();
-    final_string.push_str(&footer);
-    println!("{final_string}");
+    solution = flood[height-1].iter().sum();
+
+    let part2_time = now.elapsed().as_micros();
+    println!("Day 07 part 2: {solution} in {part2_time}µs");
 }
